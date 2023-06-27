bd5abf0d98f9f6213722071888ccccecc18f5154
Piero Stefano Márquez <89226508+PieroMarquez@users.noreply.github.com>
2023-06-27 15:59:05 -0500

    Subiendo el proyecto

---
+++ src/main/java/com/GymconnectionAPI/GymconnectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.GymconnectionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymconnectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymconnectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GymconnectionAPI/GymconnectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GymconnectionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymconnectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
