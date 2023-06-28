e88d3e9f7675fdac9117814b8b714ca1f34f3dcf
PieroMarquez <u201816402@upc.edu.pe>
2023-06-28 11:57:41 -0500

    feat: Add PaymentServiceimpl

--- src/main/java/com/GymconnectionAPI/service/PaymentService.java
+++ src/main/java/com/GymconnectionAPI/service/PaymentService.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface PaymentService {
     public abstract PaymentDto createPayment(PaymentDto paymentDto );
     public abstract List<PaymentDto> getAllPayments();
-    public abstract CustomerDto updateCustomer(Long id, CustomerDto customerDto);
+    public abstract PaymentDto updatePayment(Long id, PaymentDto paymentDto);
 }
---
+++ src/main/java/com/GymconnectionAPI/service/impl/PaymentServiceimpl.java
@@ -0,0 +1,64 @@
+package com.GymconnectionAPI.service.impl;
+
+
+import com.GymconnectionAPI.dto.PaymentDto;
+import com.GymconnectionAPI.model.Payment;
+import com.GymconnectionAPI.repository.PaymentRepository;
+import com.GymconnectionAPI.service.PaymentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PaymentServiceimpl implements PaymentService {
+    private PaymentRepository paymentRepository;
+    private ModelMapper modelMapper;
+
+    @Autowired
+    public PaymentServiceimpl(PaymentRepository paymentRepository, ModelMapper modelMapper){
+        this.paymentRepository = paymentRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public List<PaymentDto> getAllPayments(){
+        return paymentRepository.findAll()
+                .stream()
+                .map(this::EntityToDto)
+                .collect(Collectors.toList());
+    }
+
+    public PaymentDto createPayment(PaymentDto paymentDto){
+        Payment payment = DtoToEntity(paymentDto);
+        return EntityToDto(paymentRepository.save(payment));
+    }
+
+    public PaymentDto updatePayment(Long id, PaymentDto paymentDto){
+        Payment payment = paymentRepository.findById(id).orElseThrow(() -> new RuntimeException("Payment not found"));
+
+
+        if(paymentDto.getOption() !=null && !paymentDto.getOption().equals(payment.getOption())){
+            payment.setOption(paymentDto.getOption());
+        }
+        if (paymentDto.getName() != null){
+            throw new IllegalArgumentException("Name no puede ser modificado");
+        }
+        if(paymentDto.getDue_date() !=null && !paymentDto.getDue_date().equals(payment.getDue_date())){
+            payment.setDue_date(paymentDto.getDue_date());
+        }
+        if(paymentDto.getSecurity_code() !=0 && paymentDto.getSecurity_code() != payment.getSecurity_code()){
+            payment.setSecurity_code(paymentDto.getSecurity_code());
+        }
+        return EntityToDto(paymentRepository.save(payment));
+    }
+
+
+    private PaymentDto EntityToDto(Payment payment){
+        return modelMapper.map(payment, PaymentDto.class);
+    }
+    private Payment DtoToEntity(PaymentDto paymentDto){
+        return modelMapper.map(paymentDto, Payment.class);
+    }
+}
