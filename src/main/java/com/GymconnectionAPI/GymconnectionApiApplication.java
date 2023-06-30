ce4ef4e68035b549d44ca1522ab4f51d31bab985
Jeremy_134 <u202022237@upc.edu.pe>
2023-06-30 16:11:12 -0500

    Revert "feat: Swaagger - ServletInitializer"
    This reverts commit fb76983ba15a43c981666ded0e71f9c3d4d7ad27.

--- src/main/java/com/GymconnectionAPI/GymconnectionApiApplication.java
+++ src/main/java/com/GymconnectionAPI/GymconnectionApiApplication.java
@@ -3,7 +3,6 @@ package com.GymconnectionAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class GymconnectionApiApplication {
 
