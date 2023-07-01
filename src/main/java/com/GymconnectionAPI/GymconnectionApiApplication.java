913f7d7cd51d32a7fd4d2d3d943b5455efd2dffa
PieroMarquez <u201816402@upc.edu.pe>
2023-07-01 03:44:50 -0500

    fix:try 12

--- src/main/java/com/GymconnectionAPI/GymconnectionApiApplication.java
+++ src/main/java/com/GymconnectionAPI/GymconnectionApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"src/main/java/com/GymconnectionAPI/controller"})
 public class GymconnectionApiApplication {
 	public static String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
