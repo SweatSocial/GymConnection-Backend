cb8cdb8f77324a3dd976755e75a9a09616b70bc2
PieroMarquez <u201816402@upc.edu.pe>
2023-07-01 03:31:23 -0500

    fix: try 10

--- src/main/java/com/GymconnectionAPI/GymconnectionApiApplication.java
+++ src/main/java/com/GymconnectionAPI/GymconnectionApiApplication.java
@@ -1,11 +1,11 @@
-package com.GymconnectionAPI.controller;
+package com.GymconnectionAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GymconnectionApiApplication {
-	public String PORT = System.getenv("PORT");
+	public static String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(GymconnectionApiApplication.class, args);
 	}
--- src/main/java/com/GymconnectionAPI/ServletInitializer.java
+++ src/main/java/com/GymconnectionAPI/ServletInitializer.java
@@ -1,6 +1,5 @@
 package com.GymconnectionAPI;
 
-import com.GymconnectionAPI.controller.GymconnectionApiApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
